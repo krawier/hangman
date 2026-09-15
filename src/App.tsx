c843fe0b26cae5e5613f0f9782131e1c35cada94
Oliwier K <o.krawczyk06@gmail.com>
2026-09-15 19:20:07 +0200

    feat: fantastic four first steps

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,47 @@
+import { useState } from "react"
+import words from "./listOfWords.json"
+import { HangmanDrawing } from "./HangmanDrawing"
+import { HangmanWord } from "./HangmanWord"
+import { KeyBoard } from "./KeyBoard"
 
 function App() {
+
+  const [wordToGuess, setWordToGuess] = useState(() => words[Math.floor(Math.random() * words.length)])
+
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([])
+
+  return ( 
+    <div style = {{
+    maxWidth: "800px",
+    display: "flex",
+    flexDirection: "column",
+    gap: "2rem",
+    margin: "0 auto",
+    alignItems: "center",
+    }}>
+      <div style={{
+
+        fontSize:"2rem",
+        textAlign:"center",
+
+      }}>
+
+          Lose/Win
+
+      </div>
+
+      <HangmanDrawing></HangmanDrawing>
+      <HangmanWord></HangmanWord>
+      <KeyBoard></KeyBoard>
+
+
+    </div>
+  )
+
+  console.log(wordToGuess)
+  
   return <h1>Hello there!</h1>
 }
 
-export default App
 
+export default App
---
+++ src/KeyBoard.tsx
@@ -0,0 +1,3 @@
+export function KeyBoard(){
+   return null; 
+}
