6e6456f3800f6ad0af436b8782317b250f6b8191
Oliwier K <o.krawczyk06@gmail.com>
2026-09-16 18:57:12 +0200

    feat: add me

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # hangman
 A simple practice project in typescript. 
+
+timestamp -> 11:27
--- src/HangmanDrawing.tsx
+++ src/HangmanDrawing.tsx
@@ -1,9 +1,95 @@
+const HEAD = (
+  <div style={{
+    width: "50px",
+    height: "50px",
+    borderRadius: "100%",
+    border: "10px solid black",
+    position: "absolute",
+    top: "50px",
+    right: "-30px", 
+  }} />
+)
+
+const BODY = (
+  <div style={{
+    width: "10px",
+    height: "100px",
+    background: "black",
+    position: "absolute",
+    top: "120px",
+    right: 0, 
+  }} />
+)
+
+const RIGHT_ARM = (
+  <div style={{
+    width: "100px",
+    height: "10px",
+    background: "black",
+    position: "absolute",
+    top: "150px",
+    right: "-90px", 
+    rotate: "30deg",
+    transformOrigin: "left bottom",
+  }} />
+)
+
+const LEFT_ARM = (
+  <div style={{
+    width: "100px",
+    height: "10px",
+    background: "black",
+    position: "absolute",
+    top: "150px",
+    right: "0px", 
+    rotate: "-30deg",
+    transformOrigin: "right bottom",
+  }} />
+)
+
+const RIGHT_LEG = (
+  <div style={{
+    width: "130px", 
+    height: "10px",
+    background: "black",
+    position: "absolute",
+    top: "210px",
+    right: "-120px", 
+    rotate: "60deg",
+    transformOrigin: "left bottom",
+  }} />
+)
+
+const LEFT_LEG = (
+  <div style={{
+    width: "130px", 
+    height: "10px",
+    background: "black",
+    position: "absolute",
+    top: "210px",
+    right: "0px", 
+    rotate: "-60deg",
+    transformOrigin: "right bottom",
+  }} />
+)
+
 export function HangmanDrawing() {
+
   return <div style={{
 
-    position: "relative"
+    position: "relative", width: "320px",
 
   }}> 
+
+    {HEAD}
+    {BODY}
+    {RIGHT_ARM}
+    {LEFT_ARM}
+    {RIGHT_LEG}
+    {LEFT_LEG}
+
+
+    
     <div style= {{ 
     height:"10px",
     width:"200px",
